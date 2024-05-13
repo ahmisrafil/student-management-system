babfa8758ab0d3b67ac5b1d036ecd7a1e388b29f
ahmisrafil <ahmisrafil@yahoo.com>
2024-05-14 05:40:35 +0600

    api created to update the profile

--- student-management-system-server/index.js
+++ student-management-system-server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.302l1du.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,25 +40,63 @@ async function run() {
 
 
 
-    
-    app.get('/users',async (req, res) => {
-        const result = await userCollection.find().toArray();
-        res.send(result);
-      });
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
 
-      app.post('/users', async (req, res) => {
-        const user = req.body;
-        // insert email if user doesnt exists: 
-        // it can be done in many ways (1. email unique, 2. upsert 3. simple checking)
-        const query = { email: user.email }
-        const existingUser = await userCollection.findOne(query);
-        if (existingUser) {
-          return res.send({ message: 'user already exists', insertedId: null })
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // insert email if user doesnt exists: 
+      // it can be done in many ways (1. email unique, 2. upsert 3. simple checking)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('/users/:id', async (req, res) => {
+      const user = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          first_name: user.firstName,
+          last_name: user.lastName,
+          birth_date: user.dob,
+          address: user.address,
+          contact: user.mobileNumber,
+          gFirst_Name: user.gFirstName,
+          gLast_Name: user.gLastName,
+          gEmail: user.gEmail,
+          gContact: user.gMobileNumber,
+          session: user.session,
+          dept: user.dept,
+          image: user.image
         }
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-      });
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+
+
+
+
+
 
 
 
@@ -89,10 +127,10 @@ async function run() {
 run().catch(console.dir);
 
 
-  app.get('/', (req, res) => {
-    res.send("Student-management Server is Running")
-  })
-  
-  app.listen(port, () => {
-    console.log(`Student-management is running on port ${port}`);
-  })
+app.get('/', (req, res) => {
+  res.send("Student-management Server is Running")
+})
+
+app.listen(port, () => {
+  console.log(`Student-management is running on port ${port}`);
+})
