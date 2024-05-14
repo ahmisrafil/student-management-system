27959bb6617d4be655c365c954562d0d53ebe8bd
ahmisrafil <ahmisrafil@yahoo.com>
2024-05-14 23:33:20 +0600

    page  updated

--- student-management-system-client/src/Pages/Login/StudentLogin.jsx
+++ student-management-system-client/src/Pages/Login/StudentLogin.jsx
@@ -1,4 +1,4 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import bg from "../../assets/bg_img.png"
 import Swal from "sweetalert2";
 import { useContext } from "react";
